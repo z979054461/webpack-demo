873e1b87d8ec4408bf3e1d420295f1b39f15f46a
yuetx <979054461@qq.com>
2021-11-08 20:44:59 +0800

    feat: 3.development(https://webpack.docschina.org/guides/development/)

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,24 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "管理输出",
+      title: 'Development',
     }),
   ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: '/',
   },
 };
